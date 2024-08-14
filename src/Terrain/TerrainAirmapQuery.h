dbed5d281750414aa803379a7459b777ee38d113
Nick Exton <NExton@sypaq.com.au>
2024-08-14 12:15:28 +1000

    Terrain: Remove TerrainAirmapQuery class
    It's not used, and it doesn't work:
    * the Airmap API is closed down;
    * the Auterion replacement doesn't implement the 'elevation' endpoint

--- src/Terrain/TerrainAirmapQuery.h
+++ src/Terrain/TerrainAirmapQuery.h
@@ -21,41 +21,6 @@ Q_DECLARE_LOGGING_CATEGORY(TerrainAirmapQueryVerboseLog)
 
 class QGeoCoordinate;
 
-/// AirMap online implementation of terrain queries
-class TerrainAirMapQuery : public TerrainQueryInterface {
-    Q_OBJECT
-
-public:
-    TerrainAirMapQuery(QObject* parent = nullptr);
-
-    // Overrides from TerrainQueryInterface
-    void requestCoordinateHeights   (const QList<QGeoCoordinate>& coordinates) final;
-    void requestPathHeights         (const QGeoCoordinate& fromCoord, const QGeoCoordinate& toCoord) final;
-    void requestCarpetHeights       (const QGeoCoordinate& swCoord, const QGeoCoordinate& neCoord, bool statsOnly) final;
-
-private slots:
-    void _requestError              (QNetworkReply::NetworkError code);
-    void _requestFinished           (void);
-    void _sslErrors                 (const QList<QSslError> &errors);
-
-private:
-    void _sendQuery                 (const QString& path, const QUrlQuery& urlQuery);
-    void _requestFailed             (void);
-    void _parseCoordinateData       (const QJsonValue& coordinateJson);
-    void _parsePathData             (const QJsonValue& pathJson);
-    void _parseCarpetData           (const QJsonValue& carpetJson);
-
-    enum QueryMode {
-        QueryModeCoordinates,
-        QueryModePath,
-        QueryModeCarpet
-    };
-
-    QNetworkAccessManager   _networkManager;
-    QueryMode               _queryMode;
-    bool                    _carpetStatsOnly;
-};
-
 /// AirMap offline cachable implementation of terrain queries
 class TerrainOfflineAirMapQuery : public TerrainQueryInterface {
     Q_OBJECT
